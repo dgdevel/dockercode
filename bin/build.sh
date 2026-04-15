5e4f05b63f1404df4dd117a94eb44cd932d45ae1
milky <milky@archmini>
2026-04-15 21:13:09 +0200

    Dockerfile templating

--- bin/build.sh
+++ bin/build.sh
@@ -8,5 +8,12 @@ fi
 
 docker system prune -f
 
+cp Dockerfile.template Dockerfile
+if [ -f Dockerfile.extension ]; then
+  perl -i -pe 'BEGIN{local $/; $r=<STDIN>;} s/\QINCLUDED_EXTENSION\E/$r/g' Dockerfile < Dockerfile.extension
+else
+  sed -i 's/INCLUDED_EXTENSION//g' Dockerfile
+fi
+
 exec docker compose build
 
